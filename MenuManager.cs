a91d945e22d2fe1b874543546febf1d5287e322b
MysteryCallerA <mysterycallera@gmail.com>
2022-09-23 02:42:11 -0700

    Initial commit

---
+++ MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toybox;
+using Utils.style;
+
+namespace QuickMenu {
+	
+	public abstract class MenuManager {
+
+		public Point Position = Point.Zero;
+		public Style Style;
+
+		public MenuManager(Style s) {
+			Style = s;
+		}
+
+		public virtual void PressUp() {
+		}
+
+		public virtual void PressDown() {
+		}
+
+		public virtual void PressLeft() {
+		}
+
+		public virtual void PressRight() {
+		}
+
+		public virtual void PressConfirm() {
+		}
+
+		public virtual void PressCancel() {
+		}
+
+		public virtual void PushSubmenu(MenuLayer m) {
+		}
+
+		public abstract void Draw(SpriteBatch s, Camera c);
+
+	}
+}
---
+++ TreeMenu.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toybox;
+using Utils.style;
+
+namespace QuickMenu {
+	
+	public class TreeMenu:MenuManager {
+
+		public MenuLayer TopLayer;
+
+		public Stack<MenuLayer> MenuStack = new Stack<MenuLayer>();
+
+		public TreeMenu(MenuLayer top, Style s):base(s) {
+			TopLayer = top;
+		}
+
+		public MenuLayer CurrentLayer {
+			get {
+				if (MenuStack.Count == 0) return TopLayer;
+				return MenuStack.Peek();
+			}
+		}
+
+		public override void PressCancel() {
+			base.PressCancel();
+			MenuStack.Pop();
+		}
+
+		public override void PushSubmenu(MenuLayer m) {
+			base.PushSubmenu(m);
+			MenuStack.Push(m);
+		}
+
+		public override void PressConfirm() {
+			base.PressConfirm();
+			var o = CurrentLayer.GetSelectedOption();
+			if (o != null) {
+				o.Select(this);
+			}
+		}
+
+		public override void PressDown() {
+			base.PressDown();
+			CurrentLayer.PressDown();
+		}
+
+		public override void PressUp() {
+			base.PressUp();
+			CurrentLayer.PressUp();
+		}
+
+		public override void PressLeft() {
+			base.PressLeft();
+			CurrentLayer.PressLeft();
+		}
+
+		public override void PressRight() {
+			base.PressRight();
+			CurrentLayer.PressRight();
+		}
+
+		public override void Draw(SpriteBatch s, Camera c) {
+			CurrentLayer.Draw(s, c, Position, Style);
+		}
+	}
+}
