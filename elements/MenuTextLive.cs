dae8bba867c753827cedddcf3dae44c9669b446d
MysteryCallerA <mysterycallera@gmail.com>
2022-11-26 03:53:16 -0800

    Create MenuTextLive.cs

---
+++ elements/MenuTextLive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMenu.elements {
+
+	public class MenuTextLive:MenuText {
+
+		protected Func<string> Source;
+
+		public MenuTextLive(Func<string> source) {
+			Source = source;
+		}
+
+		protected internal override void FirstUpdate(MenuGroup g) {
+			var newcontent = Source.Invoke();
+			if (newcontent != Content) Content = newcontent;
+
+			base.FirstUpdate(g);
+		}
+
+	}
+}
